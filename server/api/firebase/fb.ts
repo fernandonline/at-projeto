c4cbca4c1338a6bb56de15de4004658b462b6249
fernando <luizfernandosr@outlook.com.br>
2025-07-31 23:21:55 -0300

    mongoose teste3

--- server/api/firebase/fb.ts
+++ server/api/firebase/fb.ts
@@ -2,19 +2,7 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCJmqutVSPc-CYiqbLpe1enJ5MhlQLdTS4",
 
-  authDomain: "auth-amote.firebaseapp.com",
-
-  projectId: "auth-amote",
-
-  storageBucket: "auth-amote.firebasestorage.app",
-
-  messagingSenderId: "818522531568",
-
-  appId: "1:818522531568:web:1286d3d98acb404a658435",
-
-  measurementId: "G-L9460W2Y25"
 
 };
 
